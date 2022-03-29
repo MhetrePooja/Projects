e1eb49be12bc7398818a7d57b15bf2d019e1328b
Pooja Mhetre <85214933+MhetrePooja@users.noreply.github.com>
2022-03-29 12:24:07 +0530

    added in src

---
+++ Module7/todoredux/src/components/Todo.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react'
+import {useSelector,useDispatch} from 'react-redux'
+import {addToDo,deleteToDo,removeToDo} from "../actions/index"
+import "./todo.css"
+
+const Todo=()=>{
+  const [inputData,setInputData]=useState('');
+  const list=useSelector((state)=>state.todoReducers.list);
+  const dispatch =useDispatch();
+    return(
+        <>
+        <div className="main-div">
+        <div className="child-div">
+          <figure>
+            <figcaption>Add Your List here </figcaption>
+          </figure>
+
+         <div className="addItems">
+            <input type="text" placeholder="Add items .."
+            value={inputData}
+            onChange={(event)=>setInputData(event.target.value)}/>
+            <i className="fa fa-plus add-btn" onClick={()=>dispatch(addToDo(inputData),
+              setInputData(''))}></i>
+         </div>
+
+         <div className="showItems">
+           {
+             list.map((elem)=>{
+               return(
+              <div className="eachItem" key={elem.id}>
+              <h3>{elem.data}</h3>
+                <div className="todo-btn">
+                  <i className="far fa-trash-alt add-btn" title="Delete Item" 
+                  onClick={()=>dispatch(deleteToDo(elem.id))}></i>
+                </div>
+            </div>
+               )
+             })
+           }
+         </div>
+
+         <div className="showItem">
+           <button className="btn effect04" data-sm-link-text="remove All"
+            onClick={()=> dispatch(removeToDo())}> <span>Check List</span></button>
+
+         </div>
+
+        </div>
+      </div>
+     </> 
+    
+    )
+}
+export default Todo
